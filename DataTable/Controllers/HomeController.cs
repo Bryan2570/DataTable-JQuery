f4e0ba91033e192bfac0f5ba4e71e500a19e0f39
BrayanRojasLemus <brayanrojas@wposs.com>
2023-06-09 16:35:24 -0500

    Agregar archivos de proyecto.

---
+++ DataTable/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using DataTable.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+
+namespace DataTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string cadenaSQL;
+
+        public HomeController(IConfiguration config)
+        {
+            cadenaSQL = config.GetConnectionString("CadenaSQL");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+
+        public JsonResult ListaProductos() { 
+        
+            List<Empleado> lista = new List<Empleado>();
+
+            using (var conexion = new SqlConnection(cadenaSQL)) { 
+            
+            conexion.Open();
+                var cmd = new SqlCommand("sp_listar_Productos", conexion);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader()) {
+                
+                    while (dr.Read())
+                    {
+                        lista.Add(new Empleado
+                        {
+                            IdProducto = Convert.ToInt32(dr["IdProducto"]),
+                            CodigoBarra = dr["CodigoBarra"].ToString(),
+                            Descripcion = dr["Descripcion"].ToString(),
+                            Precio = dr["Precio"].ToString()
+                        });
+                    }             
+                }
+            
+            
+            }
+            return Json(new {data = lista });
+        
+        
+        
+        
+        
+        
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DataTable/Models/Empleado.cs
@@ -0,0 +1,12 @@
+﻿namespace DataTable.Models
+{
+    public class Empleado
+    {
+
+        public int IdProducto { get; set; }
+        public string CodigoBarra { get; set; }
+        public string Descripcion { get; set; }
+        public string Precio { get; set; }
+
+    }
+}
